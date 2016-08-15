610355b08fc448b20bfd2c58124d7af96f240a04
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-08-15 23:11:25 +0530

    Initial commit

---
+++ app/src/main/java/swasthyalehar/swasthya/MainActivity.java
@@ -0,0 +1,27 @@
+package swasthyalehar.swasthya;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);//vv
+        setContentView(R.layout.activity_main);//
+    }
+    public void signin(View view)
+    {
+        Intent intent=new Intent(this,Signin.class);
+        startActivity(intent);
+    }
+    public void signout(View view)
+    {
+
+        startActivity(new Intent(this,LeaderBoardMainActivity.class));
+        /*Intent intent=new Intent(this,Signup.class);
+        startActivity(intent);*/
+    }
+}
